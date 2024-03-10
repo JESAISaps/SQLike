7a9aca3204ff5a90a4d0abe11bbf750f9936fe79
JESAISap <nicolas.strekowski@gmail.com>
2024-03-10 12:25:50 +0100

    removed unknown line

--- setup.py
+++ setup.py
@@ -3,6 +3,5 @@ import setuptools
 setuptools.setup(     
      name="SQLike",     
      version="1.0.0",
-     install_requires=["sqlite3", "python_version>=3.10"],   
-     packages=["SQLike"],
+     install_requires=["sqlite3", "python_version>=3.10"],
      )
