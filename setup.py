065fd3117a28c19f080ab1f9c9bcfa6d5f4d0228
JESAISap <nicolas.strekowski@gmail.com>
2024-03-10 12:20:55 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(     
+     name="SQLike",     
+     version="1.0.0",
+     python_requires=["sqlite3", "python_version>=3.10"],   
+     packages=["SQLike"],
+     )
