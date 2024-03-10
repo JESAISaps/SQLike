fb1fef53bb5abe714a846d6f257f82bf05d3a796
JESAISap <nicolas.strekowski@gmail.com>
2024-03-10 12:29:56 +0100

    womp womp womp

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ import setuptools
 setuptools.setup(     
      name="SQLike",     
      version="1.0.0",
-     install_requires=["sqlite3", "python_version>=3.10"],
+     packages=find_packages(),
      )
