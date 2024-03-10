159602b2bb104c94a68178d6f0ee912412cb7872
JESAISap <nicolas.strekowski@gmail.com>
2024-03-10 12:24:56 +0100

    fixed dumb mistake

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(     
      name="SQLike",     
      version="1.0.0",
-     python_requires=["sqlite3", "python_version>=3.10"],   
+     install_requires=["sqlite3", "python_version>=3.10"],   
      packages=["SQLike"],
      )
